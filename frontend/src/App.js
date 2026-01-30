f6729ca1d78272bfab75d6f6e180bef7a4526414
mariam arshad <mariamarshad137@gmail.com>
2026-01-30 20:24:24 +0500

    Initialize React frontend for notes UI

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Notes App Frontend 🚀</h1>
+      <p>React is working!</p>
+    </div>
+  );
+}
+
+export default App;
